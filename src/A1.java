37eb00c9b9adc9606c76345381d5b267b1c88f8e
Y0sh1dk <47013245+Y0sh1dk@users.noreply.github.com>
2020-09-01 21:02:34 +1000

    datafiles added

--- src/A1.java
+++ src/A1.java
@@ -1,2 +1,43 @@
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class A1 {
+    public static void main(String[] args) {
+
+        if (args.length != 1) { // If no args given, exit
+            System.out.println("Usage: A1 [file]");
+            return;
+        }
+
+        Path filePath = Paths.get(args[0]);
+        System.out.println(filePath);
+
+        if (!Files.exists(filePath)) {
+            System.out.println("File" + filePath.getFileName() + " is not found");
+            System.out.println("Exiting...");
+            return;
+        }
+
+//        If given file exits, run method "run"
+        A1 main = new A1();
+        main.run(filePath);
+
+    }
+
+    private void run(Path p) {
+
+        ArrayList<Job> jobs = new ArrayList<Job>(); // Create an ArrayList object
+
+        Job test = new Job("test", 1, 2, 3);
+
+//        jobs.add(test);
+//        System.out.println(jobs.get(0));
+
+    }
+
 }
--- src/Job.java
+++ src/Job.java
@@ -1,2 +1,24 @@
 public class Job {
+
+    private String id;
+    private int arriveTime;
+    private int execSize;
+    private int priority;
+
+    public Job() {
+        this.id = "";
+        this.arriveTime = 0;
+        this.execSize = 0;
+        this.priority = 0;
+    }
+
+    public Job(String givenID, int givenArriveTime, int givenExecSize, int givenPriority) {
+        this();
+        this.id = givenID;
+        this.arriveTime = givenArriveTime;
+        this.execSize = givenExecSize;
+        this.priority = givenPriority;
+    }
+
+
 }
